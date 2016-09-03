5fefec2019db3f816e74afea634c707baa11aca6
Kandarp <Kandarp@eduroam-ipv4-1336.triple-a.uni-kl.de>
2016-09-03 23:26:18 +0200

    File list , File read and write done. File processing pending

--- LtdFileParser/main.cpp
+++ LtdFileParser/main.cpp
@@ -5,19 +5,26 @@
 
 using namespace std;
 
+/* --------------------------------------------  0.declaration of function -------------------------------------------*/
+const string currentDateTime();
 
 int main() {
 
 /* --------------------------------------------  0.declaration of variables -------------------------------------------*/
-    string filePath = "/Users/Kandarp/ClionProjects/LtdFileParser/input/";
+    string ipfilePath = "/Users/Kandarp/ClionProjects/LtdFileParser/input/";
+    string opfilePath = "/Users/Kandarp/ClionProjects/LtdFileParser/output/";
     int selectedFileIndex;
     vector<string> fileList;
+    vector<string> ipFile;
+    vector<string> opFile;
+    string ipFileName;
+    string opFileName;
 
 
 /* --------------------------------------------  1.list out input files from given dir -------------------------------*/
     DIR *dir;
     struct dirent *ent;
-    if ((dir = opendir(filePath.c_str())) != NULL) {
+    if ((dir = opendir(ipfilePath.c_str())) != NULL) {
         /* print all the files and directories within directory */
         while ((ent = readdir(dir)) != NULL) {
             if (ent->d_name[0] != '.') {
@@ -37,35 +44,49 @@ int main() {
     }
 
 /* --------------------------------------------  2.ask user for file selection ---------------------------------------*/
-    cout << "\n Enter file number : ";
+    cout << "\nEnter file number : ";
     cin >> selectedFileIndex;
-    cout << "\n Processing file : " << fileList[selectedFileIndex - 1] << endl;
+    ipFileName = fileList[selectedFileIndex - 1];
+    cout << "\nProcessing file : " << ipFileName << endl;
 /* --------------------------------------------  3.open file and convert to lines ------------------------------------*/
-    ifstream inputFile;
-    inputFile.open(filePath + fileList[selectedFileIndex - 1]);
-
-    if (inputFile.is_open()) {
-        while (inputFile) // To get you all the lines.
-        {
-//            string tempLine;
-//            getline(inputFile, tempLine); // Saves the line in STRING.
-//            cout << tempLine; // Prints our STRING.
+    ifstream ipStream;
+    ipStream.open(ipfilePath + ipFileName);
+    string tempLine;
+    /* While there is still a line. */
+    if (ipStream.is_open()) {
+        while (getline(ipStream, tempLine)) {
+            ipFile.push_back(tempLine);
         }
-        cout << "reading file" << endl;
     } else {
-        cerr << "Unable to open file" << endl;
+        cerr << "Unable to open File" << endl;
     }
-    inputFile.close();
+    ipStream.close();
 
 /* --------------------------------------------  4.process file and check for variables  -----------------------------*/
-
-
 /* --------------------------------------------  5.ask user to enter variables ---------------------------------------*/
-
-
 /* --------------------------------------------  6.prepare output file -----------------------------------------------*/
-
-
+    opFile = ipFile;
 /* --------------------------------------------  7.create output -----------------------------------------------------*/
-    return 0;
+    // open a file in write mode.
+    ofstream opStream;
+    opFileName = currentDateTime() + "-" + ipFileName;
+    opStream.open(opfilePath + opFileName);
+    cout << "Generating output file" << endl;
+    // write inputted data into the file.
+    for (string &line : ipFile) {
+        opStream << line << endl;
+    }
+    cout << "Output generated :" << opFileName << endl;
+}
+
+
+// Get current date/time, format is YYYY-MM-DD.HH:mm:ss
+const string currentDateTime() {
+    time_t now = time(0);
+    struct tm tstruct;
+    char buf[80];
+    tstruct = *localtime(&now);
+    // for more information about date/time format
+    strftime(buf, sizeof(buf), "%d-%m-%Y.%X", &tstruct);
+    return buf;
 }
