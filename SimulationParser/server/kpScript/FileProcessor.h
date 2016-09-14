26871890d88686d90363953bb0470442e4280290
Kandarp <Kandarp@eduroam-ipv4-1312.triple-a.uni-kl.de>
2016-09-14 14:59:23 +0200

    uploaded to server

---
+++ SimulationParser/server/kpScript/FileProcessor.h
@@ -0,0 +1,65 @@
+#include <iostream>
+
+#ifndef LTDFILEPARSER_FILEPROCESSOR_H
+#define LTDFILEPARSER_FILEPROCESSOR_H
+
+using namespace std;
+
+class FileProcessor {
+private:
+    int fileIndex;
+    string fileName;
+    int lineNo;
+    string lineText;
+    string varString;
+public:
+    const string &getVarString() const;
+
+    void setVarString(const string &varString);
+
+private:
+    string varName;
+    string varType;
+    string varValue;
+    int varStartPos;
+    int varEndPos;
+public:
+    int getFileIndex() const;
+
+    void setFileIndex(int fileIndex);
+
+    const string &getFileName() const;
+
+    void setFileName(const string &fileName);
+
+    int getLineNo() const;
+
+    void setLineNo(int lineNo);
+
+    const string &getLineText() const;
+
+    void setLineText(const string &lineText);
+
+    const string &getVarName() const;
+
+    void setVarName(const string &varName);
+
+    const string &getVarType() const;
+
+    void setVarType(const string &varType);
+
+    const string &getVarValue() const;
+
+    void setVarValue(const string &varValue);
+
+    int getVarStartPos() const;
+
+    void setVarStartPos(int varStartPos);
+
+    int getVarEndPos() const;
+
+    void setVarEndPos(int varEndPos);
+
+};
+
+#endif //SIMULATIONPARSER_FILEPROCESSOR_H
