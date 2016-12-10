951103d0af729bc65fb3cc74f09ff3004e2ec8cc
Kandarp Patel <er.ikndp@gmail.com>
2016-12-10 17:51:33 +0100

    new php project added

---
+++ MoleculeDB Raw/Raw web/moleculeDB/include/nav.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-fixed-top navbar-dark bg-inverse">
+    <a class="navbar-brand" href="detail.php">Molecule DataBank</a>
+    <ul class="nav navbar-nav">
+        <li class="nav-item ">
+            <a class="nav-link" href="detail.php">Molecule List </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Update Molecule</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="addmol.php">Add New Moclecule</a>
+        </li>   x
+    </ul>
+</nav>
---
+++ MoleculeDB Raw/Raw web/moleculeDB/include/nav_login.php
@@ -0,0 +1,3 @@
+<nav class="navbar navbar-fixed-top navbar-dark bg-inverse">
+    <a class="navbar-brand" href="index.php">Molecule DataBank</a>
+</nav>
