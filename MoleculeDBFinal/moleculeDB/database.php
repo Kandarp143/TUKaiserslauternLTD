9f06eabb061fc8e3c5548f744fe00621bf1c2cfa
Kandarp <Kandarp@eduroam-ipv4-2-0024.triple-a.uni-kl.de>
2017-02-23 10:49:00 +0100

    done with changes

--- MoleculeDBFinal/moleculeDB/database.php
+++ MoleculeDBFinal/moleculeDB/database.php
@@ -14,6 +14,7 @@ class Database
 //    private static $dbUsername = 'root';
 //    private static $dbUserPassword = 'root';
 
+
     private static $cont = null;
 
     public function __construct()
