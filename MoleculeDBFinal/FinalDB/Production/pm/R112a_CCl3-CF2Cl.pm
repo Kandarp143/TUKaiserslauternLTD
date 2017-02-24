69974107e951e7ad36a7596f02c8c61018c7dbec
er.ikndp <er.ikndp@gmail.com>
2017-02-24 17:40:46 +0100

    Final DB updated

---
+++ MoleculeDBFinal/FinalDB/Production/pm/R112a_CCl3-CF2Cl.pm
@@ -0,0 +1,37 @@
+NSiteTypes =   2
+
+SiteType   =   LJ126
+NSites     =   2
+
+# S1
+x          =   0.0
+y          =   0.0
+z          =  -2.1271
+
+sigma      =   4.3651
+epsilon    = 258.25
+mass       = 101.915
+
+# S2
+x          =   0.0
+y          =   0.0
+z          =   2.1271
+
+sigma      =   4.3651
+epsilon    = 258.25
+mass       = 101.915
+
+SiteType   =   D
+NSites     =   1
+
+# S3
+x          =   0.0
+y          =   0.0
+z          =   0.0
+theta      =   0.0
+phi        =   0.0
+dipole     =   4.7132
+mass       =   0.0
+shielding  =   0.87302
+
+NRotAxes   =   auto
---
+++ MoleculeDBFinal/FinalDB/Production/pm/R134_CHF2-CHF2.pm
@@ -0,0 +1,37 @@
+NSiteTypes =   2
+
+SiteType   =   LJ126
+NSites     =   2
+
+# S1
+x          =   0.0
+y          =   0.0
+z          =  -1.5139
+
+sigma      =   3.7848
+epsilon    = 170.46
+mass       =  51.015
+
+# S2
+x          =   0.0
+y          =   0.0
+z          =   1.5139
+
+sigma      =   3.7848
+epsilon    = 170.46
+mass       =  51.015
+
+SiteType   =   Q
+NSites     =   1
+
+# S3
+x          =   0.0
+y          =   0.0
+z          =   0.0
+theta      =   0.0
+phi        =   0.0
+quadrupole =   7.8745
+mass       =   0.0
+shielding  =   0.75696
+
+NRotAxes   =   auto
